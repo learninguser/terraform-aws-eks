e28bf3811a9c58979fe6938a37e49fe05e193e2e
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-07-13 07:31:46 +0200

    Add DB module and its rules

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -1,3 +1,13 @@
+module "db" {
+  source = "git::https://github.com/daws-76s/terraform-aws-security-group.git?ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "SG for DB MySQL Instances"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "db"
+}
+
 module "ingress" {
   source         = "git::https://github.com/daws-76s/terraform-aws-security-group.git?ref=main"
   project_name   = var.project_name
@@ -53,4 +63,22 @@ resource "aws_security_group_rule" "node_ingress" {
   to_port                  = 32767
   protocol                 = "tcp"
   security_group_id        = module.node.sg_id
+}
+
+resource "aws_security_group_rule" "test" {
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.node.sg_id
+}
+
+resource "aws_security_group_rule" "allow_everyone" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.node.sg_id
+  security_group_id = module.db.sg_id
 }
--- 02-sg/parameters.tf
+++ 02-sg/parameters.tf
@@ -15,3 +15,9 @@ resource "aws_ssm_parameter" "ingress_sg_id" {
   type  = "String"
   value = module.ingress.sg_id
 }
+
+resource "aws_ssm_parameter" "db_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/db_sg_id"
+  type  = "String"
+  value = module.db.sg_id
+}
